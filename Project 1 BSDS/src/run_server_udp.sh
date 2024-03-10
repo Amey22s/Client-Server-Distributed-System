e13f26ab0c7e54559956a8ba5faa8d16e5042173
Amey Gurunath Parab <ameygurunathparab@gmail.com>
2024-03-09 23:52:21 -0500

    fixed readme image issue

--- Project 1 BSDS/src/run_server_udp.sh
+++
@@ -1,9 +0,0 @@
-PROJECT_NETWORK='TCP_UDP'
-SERVER_IMAGE='udp-server'
-SERVER_CONTAINER='my-udp-server'
-# run the image and open the required ports
-echo "----------Running sever app----------"
-docker run --rm -d -p 9001:9001/udp  --name $SERVER_CONTAINER --network $PROJECT_NETWORK $SERVER_IMAGE
-
-echo "----------watching logs from server----------"
-docker logs $SERVER_CONTAINER -f
